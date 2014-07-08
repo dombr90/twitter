f91d2b89d23a3898e275f4cf6f72b4c50b247c20
Shingis Kozaydar <shingis90@gmail.com>
2014-07-08 08:54:03 +0000

    add a staticPages controller

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140704135243) do
+ActiveRecord::Schema.define(version: 20140707142829) do
 
   create_table "posts", force: true do |t|
     t.string   "title"
     t.string   "body"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "users", force: true do |t|
+    t.string   "name"
+    t.string   "email"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
